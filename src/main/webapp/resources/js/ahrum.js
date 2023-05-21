21a702cc67e41302bf6b896d32a7b2a981c5520a
AhrumKim <ahrum73@gmail.com>
2023-05-21 23:29:42 +0900

    ahrum 최종

--- src/main/webapp/resources/js/ahrum.js
+++ src/main/webapp/resources/js/ahrum.js
@@ -17,35 +17,34 @@ function stopShake() {
 
 document.getElementById('fortune-button').addEventListener('click', showFortune);
 
+  
 function showFortune() {
-  var fortunes = [
-    "오늘은 행운이 함께할 것입니다.",
-    "긍정적인 마음으로 진행하세요, 좋은 결과를 얻을 수 있습니다.",
-    "즐거운 일이 생길 것입니다."
-  ];
+  // section2로 이동
+  window.location.href = "#section2";
+} 
 
-  var fortuneIndex = Math.floor(Math.random() * fortunes.length);
-  var fortune = fortunes[fortuneIndex];
 
-  document.getElementById('result').textContent = fortune;
-  document.getElementById('result').style.display = 'block';
-}    
-        // 이미지 지도에 표시할 마커입니다
-var marker = {
-    position: new kakao.maps.LatLng( 37.579445, 126.9699761), 
-    text: '서울특별시 종로구 창경궁로 254 7층' // text 옵션을 설정하면 마커 위에 텍스트를 함께 표시할 수 있습니다
-};
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+  mapOption = { 
+        center: new kakao.maps.LatLng( 37.583782851238425, 126.99998018715789), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    }; 
 
-var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지도를 표시할 div
-    staticMapOption = { 
-        center: new kakao.maps.LatLng( 37.579445, 126.9699761), // 이미지 지도의 중심좌표
-        level: 4, // 이미지 지도의 확대 레벨
-        marker: marker // 이미지 지도에 표시할 마커
-    };
-    
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// 이미지 지도를 생성합니다
-var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+// 커스텀 오버레이에 표시할 내용입니다     
+// HTML 문자열 또는 Dom Element 입니다 
+var content = '<div class ="label"><span class="left"></span><span class="center">KOSA 혜화교육센터</span><span class="right"></span></div>';
 
-marker.setDraggable(true); 
+// 커스텀 오버레이가 표시될 위치입니다 
+var position = new kakao.maps.LatLng( 37.583782851238425, 126.99998018715789);  
+
+// 커스텀 오버레이를 생성합니다
+var customOverlay = new kakao.maps.CustomOverlay({
+    position: position,
+    content: content   
+});
+
+// 커스텀 오버레이를 지도에 표시합니다
+customOverlay.setMap(map);
 
